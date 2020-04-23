b21c7e08b57473aa08bb85a656b47809b93bb137
Mark McConachie <markm1206@tamu.edu>
2020-04-23 10:12:10 -0500

    twitter working local

--- server/JsonServer.js
+++ server/JsonServer.js
@@ -1,25 +1,36 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-twttr = require('./TwitterMash')
+twttr = require('./TwitterConnect')
 var jsonData;
 
 http.createServer(function (request, response) {
     console.log('request starting...');
     response.setHeader('Access-Control-Allow-Origin', '*');
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './index.html';
-    //response.writeHead("Access-Control-Allow-Origin", "*");
-    var extname = path.extname(filePath);
+    
+    var searchTerm = request.url.substr(1);
+    console.log(searchTerm)
+
+    
     var contentType = 'application/json';
     response.writeHead(200, { 'Content-Type': contentType });
-    twttr = require('./TwitterMash')
-    twttr.twitHandle.get('search/tweets', { q: 'result_type=popular', count: 3 }, function(err,data,res) {
-        console.log(data)
-        //console.log(Object.getOwnPropertyNames(data))
-        response.end(JSON.stringify(data));
-    })
+    twttr = require('./TwitterConnect')
+    if(searchTerm == "$Trending")
+    {
+        twttr.twitHandle.get('search/tweets', { q: 'covid', count: 3 }, function(err,data,res) {
+            console.log(data)
+            //console.log(Object.getOwnPropertyNames(data))
+            response.end(JSON.stringify(data));
+        })
+    }
+    else{
+        twttr.twitHandle.get('search/tweets', { q: searchTerm, count: 3 }, function(err,data,res) {
+            console.log(data)
+            //console.log(Object.getOwnPropertyNames(data))
+            response.end(JSON.stringify(data));
+        })
+    }
+        
     
     
     
