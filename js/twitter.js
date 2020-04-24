a9eb690ee623b9a3609935d4e2dc0370d0876309
leonplust <leonplust@gmail.com>
2020-04-23 20:47:18 -0500

    remove garbage code

--- js/twitter.js
+++ js/twitter.js
@@ -63,42 +63,6 @@ function twitSearch(searchTerm){
   request.send()
 }
 
-function twitRefresh(){
-  var container = getElementById("contentDeck");
-
-  var request = new XMLHttpRequest()
-
-  // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET','http://twitudit.herokuapp.com/twitter/'+searchTerm+'/5', true)
-  request.onload = function() {
-    //console.log(request.status)
-    // Begin accessing JSON data here
-    if(request.status == 200){
-      
-      console.log(this.response);
-      var data = JSON.parse(this.response)
-      console.log(data.statuses)
-      for(i in data.statuses){
-        //console.log(i);
-        //var tweet = document.getElementById("tweet"+i);
-        //tweet.innerHTML = "";
-        container.innerHTML+='<div class="col-md-4"><div class="card mb-4 shadow-sm">'
-        console.log(data.statuses[i].id_str)
-        tweet.setAttribute("tweetID",data.statuses[i].id_str)
-        var id = tweet.getAttribute("tweetID");
-        twttr.widgets.createTweet(id, tweet)
-      }
-    }
-    //
-    //twttr.widgets.load()
-    //console.log(this.response)
-  }
-  // Send request
-  request.send()
-
-
-}
-
 function fillTweet(tweet,tweetNum){
   var request = new XMLHttpRequest()
 
